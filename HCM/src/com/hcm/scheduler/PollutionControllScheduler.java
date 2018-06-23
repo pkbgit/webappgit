d9c52fd9a4ad26d8d7cd7400a676b275ac06c0fe
pkbgit <prakashkbehera@gmail.com>
2018-06-23 18:28:03 +0530

    Package created and java classs PollutionControllScheduler is added in hcm project

---
+++ HCM/src/com/hcm/scheduler/PollutionControllScheduler.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.hcm.scheduler;
+
+/**
+ * @author PRAKASH
+ *
+ */
+public class PollutionControllScheduler {
+
+	/**
+	 * 
+	 */
+	public PollutionControllScheduler() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
